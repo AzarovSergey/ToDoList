6766ce7b09905ac06c9f911d145820f4510b8b33
Azarov Sergey <108asv@mail.ru>
2016-02-21 18:40:27 +0300

    Create repositories for to-do lists, items and folders.

---
+++ ToDoList/BLL.Interface/Services/IFolderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interface.Entities;
+
+namespace BLL.Interface.Services
+{
+    public interface IFolderService
+    {
+        void Create(FolderEntity folder);
+        IEnumerable<FolderEntity> GetByAuthorId(int authorId);
+    }
+}
