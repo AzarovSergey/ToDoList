f2c9a48d9eed3b550917be617ecbd52349a6798d
Azarov Sergey <108asv@mail.ru>
2016-03-09 20:42:30 +0300

    Add IEntity

--- ToDoList/Epam.Wunderlist.Web/Controllers/FolderController.cs
+++ ToDoList/Epam.Wunderlist.Web/Controllers/FolderController.cs
@@ -44,48 +44,6 @@ namespace Epam.Wunderlist.Web.Controllers.API
             return Json(folders, JsonRequestBehavior.AllowGet);
         }
 
-        [HttpGet]
-        [AllowAnonymous]
-        public JsonResult/*<IEnumerable<FolderModel>>*/ GetFillFolders()
-        {
-            throw new NotImplementedException();
-            //return Json
-            //    (new FolderModel()
-            //    {
-            //        Name ="f1",
-            //        OrderIndex =3,
-            //        ToDoLists =
-            //            new ToDoListModel[]
-            //            {
-            //            new ToDoListModel
-            //                {
-            //                    Name ="tl1",OrderIndex=2,
-            //                    Items =new ItemModel[] { new ItemModel() {Name="ii1" },new ItemModel {Name="ii2" } }
-            //                }
-            //            }
-            //    },
-            //    JsonRequestBehavior.AllowGet);
-            if (!User.Identity.IsAuthenticated)
-            {
-                return Json(new { redirect = "/account/login/" }, JsonRequestBehavior.AllowGet);
-            }
-            var user = userService.GetByEmail(User.Identity.Name);
-            FolderEntity[] folders = folderService.GetByAuthorId(user.Id).ToArray();
-            FolderModel[] folderModels=new FolderModel[folders.Count()];
-            for (int i = 0; i < folders.Length; i++)
-            {
-                folderModels[i] = folders[i].ToFolderModel();
-
-                ToDoListEntity[] toDoLists = toDoListService.GetByFolderId(folders[i].Id).ToArray();
-                ToDoListModel[] toDoListModels = new ToDoListModel[toDoLists.Length];
-                for (int j = 0; j < toDoLists.Length; j++)
-                {
-                    toDoListModels[j].Items = itemService.GetByToDoListId(toDoLists[j].Id).Select(item=>item.ToItemModel()).ToArray();
-                }
-                folderModels[i].ToDoLists = toDoListModels;
-            }
-            return Json(folderModels, JsonRequestBehavior.AllowGet);
-        }
 
     }
 }
